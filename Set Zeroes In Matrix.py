9ea19ffe39ec53253e50877824ec92a8cd676976
Atul Manohar Suren <130427760+AtulSuren@users.noreply.github.com>
2023-07-04 11:50:45 +0530

    Update Set Zeroes In Matrix.py

--- Set Zeroes In Matrix.py
+++ Set Zeroes In Matrix.py
@@ -1,24 +1,19 @@
 def setZeroes(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
-
-    
     zerorows = set()
     zerocols = set()
 
-    
     for i in range(rows):
         for j in range(cols):
             if matrix[i][j] == 0:
                 zerorows.add(i)
                 zerocols.add(j)
-
-    
+                
     for row in zerorows:
         for j in range(cols):
             matrix[row][j] = 0
 
-    
     for col in zerocols:
         for i in range(rows):
             matrix[i][col] = 0
